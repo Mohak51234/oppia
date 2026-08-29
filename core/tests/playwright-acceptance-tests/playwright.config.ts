fa1551a7b0128140153af86e993518c30c34b76d
Mohak <143473709+Mohak51234@users.noreply.github.com>
2026-08-29 22:26:46 +0530

    tmp

--- core/tests/playwright-acceptance-tests/playwright.config.ts
+++ core/tests/playwright-acceptance-tests/playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
   use: {
     baseURL: 'http://localhost:8181',
     headless: process.env.HEADLESS !== 'false',
-    trace: 'retain-on-failure',
+    trace: 'on',
     screenshot: 'only-on-failure',
     video: 'on-first-retry',
   },
